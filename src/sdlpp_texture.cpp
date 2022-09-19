a7063a3f72bc8941dcfddc001dc898299436a2ea
Zeldruck <felix.jouannneau@gmail.com>
2022-09-19 18:41:29 +0200

    Texture

--- src/sdlpp_texture.cpp
+++ src/sdlpp_texture.cpp
@@ -2,10 +2,9 @@
 #include "sdlpp_renderer.hpp"
 #include "sdlpp_surface.hpp"
 
-SDLpp_texture::SDLpp_texture(SDLpp_texture&& _texturepp)
+SDLpp_texture::SDLpp_texture(SDLpp_texture&& _texturepp) noexcept
 {
-	m_texture = _texturepp.m_texture;
-	_texturepp.m_texture = nullptr;
+	std::swap(m_texture, _texturepp.m_texture);
 }
 
 SDLpp_texture::~SDLpp_texture()
@@ -14,17 +13,38 @@ SDLpp_texture::~SDLpp_texture()
 		SDL_DestroyTexture(m_texture);
 }
 
+SDLpp_texture& SDLpp_texture::operator=(SDLpp_texture&& _texturepp) noexcept
+{
+	std::swap(m_texture, _texturepp.m_texture);
+
+	return *this;
+}
+
 SDLpp_texture SDLpp_texture::LoadFromFile(SDLpp_renderer& _renderer, const std::string& _filepath)
 {
-	SDLpp_surface surface(_filepath);
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(_renderer.GetHandle(), surface.GetHandle());
+	return LoadFromSurface(_renderer, SDLpp_surface::LoadFromFile(_filepath));
+}
+
+SDLpp_texture SDLpp_texture::LoadFromSurface(SDLpp_renderer& _renderer, const SDLpp_surface& _surface)
+{
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(_renderer.GetHandle(), _surface.GetHandle());
 
 	return SDLpp_texture(texture);
 }
 
-SDL_Texture* SDLpp_texture::GetHandle()
+SDL_Texture* SDLpp_texture::GetHandle() const
 {
 	return m_texture;
+}
+
+SDL_Rect SDLpp_texture::GetRect() const
+{
+	SDL_Rect rect;
+	rect.x = 0;
+	rect.y = 0;
+	SDL_QueryTexture(m_texture, nullptr, nullptr, &rect.w, &rect.h);
+
+	return rect;
 }
 
 SDLpp_texture::SDLpp_texture(SDL_Texture* _texture) :
