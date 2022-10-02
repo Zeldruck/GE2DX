9b425b23969e579d79137e8ce25a817e5ff8cfef
Zeldruck <felix.jouannneau@gmail.com>
2022-10-02 15:20:37 +0200

    Input system

---
+++ include/InputSystem.hpp
@@ -0,0 +1,44 @@
+#ifndef _INPUTSYSTEM_HPP_
+#define _INPUTSYSTEM_HPP_
+
+#include <memory>
+#include <iostream>
+#include <functional>
+#include <map>
+
+class SEvent;
+
+class InputSystem
+{
+public:
+	InputSystem();
+	~InputSystem();
+
+	InputSystem(InputSystem&) = delete;
+	void operator=(const InputSystem&) = delete;
+
+	static std::shared_ptr<InputSystem> Instance();
+
+	void BindKeyPressed(int, const std::string&);
+	void BindMouseButton(int, const std::string&);
+
+	void OnAction(const std::string&, std::function<void()>);
+
+private:
+	static std::shared_ptr<InputSystem> instance;
+
+	std::map<std::string, std::shared_ptr<SEvent>> actions;
+
+
+};
+
+struct SEvent
+{
+	std::string name;
+	int keyButton;
+	int mouseButton;
+
+	std::function<void()> action;
+};
+
+#endif // !_INPUTSYSTEM_HPP_
